9e376a0a5578c3ee985a55bbe5e601cee54220e8
tyage <namatyage@gmail.com>
2015-05-01 17:49:38 +0900

    画像複数生成してcsvに出す

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 var captchagen = require('captchagen');
 var fs = require('fs');
 
-var outputDir = './output/';
-var captcha = captchagen.create();
-var filePath = outputDir + captcha.text() + '.png';
-console.log(filePath);
+var dataFilePath = './output/data.csv';
+var imageOutputDir = './output/images/';
+var imageUrlPrefix = 'http://localhost:3000/';
+var times = +process.argv[2];
 
-captcha.generate();
+// generate images
+var captchas = [];
+for (var i = 0; i < times; ++i) {
+  var captcha = captchagen.create();
+  var filePath = imageOutputDir + i.toString() + '.png';
+  console.log(captcha.text(), filePath);
 
-captcha.buffer(function(e, buffer) {
-  fs.writeFile(filePath, buffer, function() {
-    console.log('done');
+  captcha.generate();
+
+  captcha.buffer((e, buffer) => {
+    fs.writeFile(filePath, buffer);
   });
-});
+
+  captchas.push(captcha);
+}
+
+// generate csv
+var header = ['image', 'answer'].join(', ');
+var body = captchas.map((captcha, i) => [
+  imageUrlPrefix + i.toString() + '.png',
+  captcha.text()
+].join(', '));
+var data = header + '\n' + body.join('\n');
+fs.writeFile(dataFilePath, data);
